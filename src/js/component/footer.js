11b430c617040dcf347998fda12e74472ff31bc7
jgustavoj <gjime011@fiu.edu>
2020-11-15 02:05:26 +0000

    ..

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,18 +3,25 @@ import { Link } from "react-router-dom";
 import logo from "../../img/logo.png";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<div>
-			{/* this needs to go all the way to the left when styling */}
-			{/* <span className="navbar-brand mb-0">Logo</span> */}
-			<img className="img-fluid" src={logo} height={45} width={45} />
-			<span className="navbar-brand mb-0 h1">Mobile App</span>
-			<span className="navbar-brand mb-0 h1">Company</span>
-			<span className="navbar-brand mb-0 h1">Contact Us</span>
-			{/* this needs to go all the way to the right when styling */}
-			<span className="navbar-brand mb-0 ">We love our users!</span>
+	<>
+		<div className="container">
+			<footer className="footer mt-auto py-3 text-center">
+				<div>
+					{/* this needs to go all the way to the left when styling */}
+					{/* <span className="navbar-brand mb-0">Logo</span> */}
+					<img className="img-fluid float-left" src={logo} height={45} width={45} />
+					<span className="navbar-brand mb-0 h1">Mobile App</span>
+					<span className="navbar-brand mb-0 h1">Company</span>
+					<span className="navbar-brand mb-0 h1">Contact Us</span>
+					{/* this needs to go all the way to the right when styling */}
+					<span className="navbar-brand mb-0 float-right">
+						{" "}
+						<i className="fas fa-heart">We love our users!</i>
+					</span>
+				</div>
+			</footer>
 		</div>
-	</footer>
+	</>
 );
 
 {
