c30dc81f39fc507ac68710b78e10e862d34e95fb
jgustavoj <gjime011@fiu.edu>
2020-12-08 18:22:45 +0000

    nav bar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,35 +5,37 @@ import { Context } from "../store/appContext";
 
 export const Navbar = () => {
 	const { store, actions } = useContext(Context);
-	const notify = () => {
-		fetch(
-			"https://3000-a1007fd4-1ba0-4ea4-a40d-d94e9227503b.ws-us02.gitpod.io/notification",
+	// const notify = () => {
+	// 	fetch(
+	// 		"https://3000-a1007fd4-1ba0-4ea4-a40d-d94e9227503b.ws-us02.gitpod.io/notification",
 
-			{
-				method: "DELETE",
-				headers: {
-					"Content-Type": "application/json"
-				}
-			}
-		)
-			.then(function(response) {
-				if (!response.ok) {
-					throw Error(response.statusText);
-				}
-				return response.json();
-			})
-			.then(function(responseAsJson) {
-				console.log("responseAsJson", responseAsJson);
-			})
-			.catch(function(error) {
-				console.log("Looks like there was a problem: \n", error);
-			});
-	};
+	// 		{
+	// 			method: "DELETE",
+	// 			headers: {
+	// 				"Content-Type": "application/json"
+	// 			}
+	// 		}
+	// 	)
+	// 		.then(function(response) {
+	// 			if (!response.ok) {
+	// 				throw Error(response.statusText);
+	// 			}
+	// 			return response.json();
+	// 		})
+	// 		.then(function(responseAsJson) {
+	// 			console.log("responseAsJson", responseAsJson);
+	// 		})
+	// 		.catch(function(error) {
+	// 			console.log("Looks like there was a problem: \n", error);
+	// 		});
+	// };
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light fixed-top mb-20" id="mainNav">
-			<span className="navbar-brand mb-0 h1" onClick={() => notify()}>
-				<img src={logo} height={50} width={50} />
-			</span>
+			<Link to="/">
+				<span className="navbar-brand mb-0 h1" /*onClick={() => notify()}*/>
+					<img src={logo} height={50} width={50} />
+				</span>
+			</Link>
 			{/* <span className="navbar-brand mb-0 h1">About</span>
 				<span className="navbar-brand mb-0 h1">How it works</span> */}
 
